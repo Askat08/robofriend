30a369034f8603a4f05dd158acb6f537fddcd6a3
Askat Bakyev <bakyev1991@gmail.com>
2020-01-06 21:50:55 -0500

    Updates

---
+++ precache-manifest.2118c7a5447bf7746f1aef4c649f17a2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d94e57470f5bd10a6d4da297541b55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2c120c1f5615ba849dc",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7b607cc7b842e94febdb",
+    "url": "/static/css/main.d8704f77.chunk.css"
+  },
+  {
+    "revision": "f2c120c1f5615ba849dc",
+    "url": "/static/js/2.64a50d72.chunk.js"
+  },
+  {
+    "revision": "d3fe802f521069c533c2d93f4d5bace1",
+    "url": "/static/js/2.64a50d72.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b607cc7b842e94febdb",
+    "url": "/static/js/main.4dc13a50.chunk.js"
+  },
+  {
+    "revision": "aa6fed8716c2be098a58",
+    "url": "/static/js/runtime-main.55a556ca.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
